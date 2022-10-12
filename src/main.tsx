774daa62a480165d1439bf65af37e07a52a96048
mateusanjost <mateusanjosmgb@gmail.com>
2022-10-12 01:00:49 -0300

    add error-page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import ErrorPage from "./pages/error-page";
 import Index from "./pages/Index";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Index />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
